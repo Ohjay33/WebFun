db98d6122eb6a9d838b4acfcee794e57569d8248
Oscar Moore <ohjaymoore@gmail.com>
2022-05-03 11:48:57 -0500

    Conditional-Practice

---
+++ Dojo assignments/Pre _course/algo.js
@@ -0,0 +1,22 @@
+var height = 52
+if(height>52) {
+    console.log("Get on that ride kiddo!")
+}
+else{
+    console.log("sorry kiddo.Maybe next year")
+}
+
+
+var height=52
+var age=18
+
+if(height>=52){
+    console.log("Get on ride kiddo")
+    if (age>18){
+        console.log("Get on ride kiddo")
+        else{
+            console.log("sorry kiddo. Maybe next time")
+        }
+    }
+}
+
