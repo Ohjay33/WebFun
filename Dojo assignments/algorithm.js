adcf84be267111f5906027e9f80b20bf50d1ec34
Oscar Moore <ohjaymoore@gmail.com>
2022-05-23 10:40:51 -0500

    Initial1

--- Dojo assignments/algorithm.js
+++ Dojo assignments/algorithm.js
@@ -7,33 +7,78 @@
 // for(var i=0; i<numbers.length;i++){
 //     if(numbers[i] > 0){
 //         countPositives++;
-        
+
 //     }
 // }
-    
-    
+
+
 // console.log("there are " + countPositives + " positive values");
 
 
 
 // var x = 5;
-    
+
 // function setX(newValue) {
 //     x = newValue; 
 // }
-    
+
 // console.log(x);
 // setX(15);
 // console.log(x);
 
-// var x = 5;
-    
-// function addToX(amount) {
+
+// var x = 20;
+
+// function dog(amount) {
 //     return x + amount;
 //     console.log("hello there");
 // }
-    
+
 // console.log(x);
-// var result = addToX(-10);
+// var result = dog(-10);
 // console.log(result);
 // console.log(x);
+
+
+
+// Code for basic loop//
+
+// for(var i=0; i<80; i++) {
+//     console.log(i);
+// }
+
+// function say("message"){
+
+
+
+  var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
+
+
+
+  var lifeAnswers = answers[Math.floor(Math.random() * answers.length)];
+
+console.log(answer);
+
+   
