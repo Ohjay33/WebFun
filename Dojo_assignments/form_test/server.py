6b1a12fa62914265eeeded2c67d5e3ac9ba99b43
Oscar Moore <ohjaymoore@gmail.com>
2022-06-14 12:41:32 -0500

    Initial1

---
+++ Dojo_assignments/form_test/server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("index1.html")
+if __name__ == "__main__":
+    app.run(debug=True)
+
