08070d6f89cde30937e2da92e6804f316a468b1a
Oscar Moore <ohjaymoore@gmail.com>
2022-07-11 05:49:50 -0500

    Initial1

---
+++ Dojo_assignments/mern/notes.jsx
@@ -0,0 +1,14 @@
+// starting a new project "npx create-react-app-"folder name"
+// run react "npm start"
+
+
+
+
+
+// basic functional component setup
+import React from "react";
+
+const form = ()=>{}
+
+
+export default form;
